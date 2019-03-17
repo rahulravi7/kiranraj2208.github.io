6c8d25b2ab8ab1606fe4e292209557559d33e600
Kiran Raj H K <32378526+kiranraj2208@users.noreply.github.com>
2019-03-17 12:25:46 +0530

    Update ticJs.js

--- ticJs.js
+++ ticJs.js
@@ -100,32 +100,9 @@ document.addEventListener("DOMContentLoaded", function(){
 }
     function autoPlay(){
         var toTick = 0;
-        if(ticked[1] == 1 && ticked[2] == 1 && ticked[3] == 0) toTick = 3;
-        else if(ticked[2] == 1 && ticked[3] == 1 && ticked[1] == 0) toTick = 1;
-        else if(ticked[1] == 1 && ticked[3] == 1 && ticked[2] == 0) toTick = 2;
-        else if(ticked[4] == 1 && ticked[5] == 1 && ticked[6] == 0) toTick = 6;
-        else if(ticked[5] == 1 && ticked[6] == 1 && ticked[4] == 0) toTick = 4;
-        else if(ticked[4] == 1 && ticked[6] == 1 && ticked[5] == 0) toTick = 5;
-        else if(ticked[7] == 1 && ticked[8] == 1 && ticked[9] == 0) toTick = 9;
-        else if(ticked[7] == 1 && ticked[9] == 1 && ticked[8] == 0) toTick = 8;
-        else if(ticked[8] == 1 && ticked[9] == 1 && ticked[7] == 0) toTick = 7;
-        else if(ticked[1] == 1 && ticked[4] == 1 && ticked[7] == 0) toTick = 7;
-        else if(ticked[4] == 1 && ticked[7] == 1 && ticked[1] == 0) toTick = 1;
-        else if(ticked[1] == 1 && ticked[7] == 1 && ticked[4] == 0) toTick = 4;
-        else if(ticked[2] == 1 && ticked[5] == 1 && ticked[8] == 0) toTick = 8;
-        else if(ticked[2] == 1 && ticked[8] == 1 && ticked[5] == 0) toTick = 5;
-        else if(ticked[5] == 1 && ticked[8] == 1 && ticked[2] == 0) toTick = 2;
-        else if(ticked[3] == 1 && ticked[6] == 1 && ticked[9] == 0) toTick = 9;
-        else if(ticked[3] == 1 && ticked[9] == 1 && ticked[6] == 0) toTick = 6;
-        else if(ticked[6] == 1 && ticked[9] == 1 && ticked[3] == 0) toTick = 3;
-        else if(ticked[1] == 1 && ticked[5] == 1 && ticked[9] == 0) toTick = 9;
-        else if(ticked[1] == 1 && ticked[9] == 1 && ticked[5] == 0) toTick = 5;
-        else if(ticked[5] == 1 && ticked[9] == 1 && ticked[1] == 0) toTick = 1;
-        else if(ticked[3] == 1 && ticked[5] == 1 && ticked[7] == 0) toTick = 7;
-        else if(ticked[3] == 1 && ticked[7] == 1 && ticked[5] == 0) toTick = 5;
-        else if(ticked[7] == 1 && ticked[5] == 1 && ticked[3] == 0) toTick = 3;
+        
         //----------------
-        else if(ticked[1] == 2 && ticked[2] == 2 && ticked[3] == 0) toTick = 3;
+         if(ticked[1] == 2 && ticked[2] == 2 && ticked[3] == 0) toTick = 3;
         else if(ticked[2] == 2 && ticked[3] == 2 && ticked[1] == 0) toTick = 1;
         else if(ticked[1] == 2 && ticked[3] == 2 && ticked[2] == 0) toTick = 2;
         else if(ticked[4] == 2 && ticked[5] == 2 && ticked[6] == 0) toTick = 6;
@@ -150,7 +127,30 @@ document.addEventListener("DOMContentLoaded", function(){
         else if(ticked[3] == 2 && ticked[7] == 2 && ticked[5] == 0) toTick = 5;
         else if(ticked[7] == 2 && ticked[5] == 2 && ticked[3] == 0) toTick = 3;
         //--------------------------
-        
+        else if(ticked[1] == 1 && ticked[2] == 1 && ticked[3] == 0) toTick = 3;
+        else if(ticked[2] == 1 && ticked[3] == 1 && ticked[1] == 0) toTick = 1;
+        else if(ticked[1] == 1 && ticked[3] == 1 && ticked[2] == 0) toTick = 2;
+        else if(ticked[4] == 1 && ticked[5] == 1 && ticked[6] == 0) toTick = 6;
+        else if(ticked[5] == 1 && ticked[6] == 1 && ticked[4] == 0) toTick = 4;
+        else if(ticked[4] == 1 && ticked[6] == 1 && ticked[5] == 0) toTick = 5;
+        else if(ticked[7] == 1 && ticked[8] == 1 && ticked[9] == 0) toTick = 9;
+        else if(ticked[7] == 1 && ticked[9] == 1 && ticked[8] == 0) toTick = 8;
+        else if(ticked[8] == 1 && ticked[9] == 1 && ticked[7] == 0) toTick = 7;
+        else if(ticked[1] == 1 && ticked[4] == 1 && ticked[7] == 0) toTick = 7;
+        else if(ticked[4] == 1 && ticked[7] == 1 && ticked[1] == 0) toTick = 1;
+        else if(ticked[1] == 1 && ticked[7] == 1 && ticked[4] == 0) toTick = 4;
+        else if(ticked[2] == 1 && ticked[5] == 1 && ticked[8] == 0) toTick = 8;
+        else if(ticked[2] == 1 && ticked[8] == 1 && ticked[5] == 0) toTick = 5;
+        else if(ticked[5] == 1 && ticked[8] == 1 && ticked[2] == 0) toTick = 2;
+        else if(ticked[3] == 1 && ticked[6] == 1 && ticked[9] == 0) toTick = 9;
+        else if(ticked[3] == 1 && ticked[9] == 1 && ticked[6] == 0) toTick = 6;
+        else if(ticked[6] == 1 && ticked[9] == 1 && ticked[3] == 0) toTick = 3;
+        else if(ticked[1] == 1 && ticked[5] == 1 && ticked[9] == 0) toTick = 9;
+        else if(ticked[1] == 1 && ticked[9] == 1 && ticked[5] == 0) toTick = 5;
+        else if(ticked[5] == 1 && ticked[9] == 1 && ticked[1] == 0) toTick = 1;
+        else if(ticked[3] == 1 && ticked[5] == 1 && ticked[7] == 0) toTick = 7;
+        else if(ticked[3] == 1 && ticked[7] == 1 && ticked[5] == 0) toTick = 5;
+        else if(ticked[7] == 1 && ticked[5] == 1 && ticked[3] == 0) toTick = 3;
         else if(ticked[5] == 0){
             toTick = 5;
         }
